4eff59bb440e6fa047c399ae531dcacef2715e7f
Ricamslv <solivaricamae@gmail.com>
2023-03-31 00:04:14 +0900

    tailwind css

--- google-clone-nextjs13/google-clone/src/app/head.jsx
+++ google-clone-nextjs13/google-clone/src/app/head.jsx
@@ -7,7 +7,7 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1"
       name="viewport" />
       <meta name="description" content="Google clone created by Next js 13" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.svg" />
       </>
     )
 }
